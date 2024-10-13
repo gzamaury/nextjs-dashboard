6041aea7d8ba4a484f7689888f1424ff3ef51d63
Amaury Gz <gzamaury@hotmail.com>
2024-10-13 05:14:48 +0000

    Implementing Partial Prerendering - https://nextjs.org/learn/dashboard-app/partial-prerendering#implementing-partial-prerendering

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    // Enabling PPR (Version 14) - https://nextjs.org/docs/canary/app/building-your-application/rendering/partial-prerendering#enabling-ppr-version-14
+    // ppr: true,
+    
+    ppr: 'incremental',
+  },
+};
 
 export default nextConfig;
